76393674957120f54b6c43d07baf8b4ba871cb9f
Augustine <Augustine.m@revealhealthtech.com>
2024-06-27 18:25:54 +0530

    Bugfixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import {
-  COMPLETED,
-  COURSEDETAILINDEX,
-  COURSESBYCATEGORY,
-  ENROLLED,
-  PENDING,
-  createAPIEndpoint,
+	COMPLETED,
+	COURSEDETAILINDEX,
+	COURSESBYCATEGORY,
+	ENROLLED,
+	PENDING,
+	createAPIEndpoint,
 } from "@/lib/constants";
 import { useEffect, useMemo, useRef, useState } from "react";
 import AllCourses from "../components/AllCourses";
@@ -18,146 +18,172 @@ import { useSidebar } from "../context/SideBarContext";
 import CertificationsDashboard from "../components/CertificationsDashboard";
 
 export default function Dashboard() {
-  const { position, data, updateSideBar } = useSidebar();
-  const id = useRef("");
-  const catImage = useRef("");
-  const [mainHeader, setMainHeader] = useState("Dashboard");
-  const [secondaryHeader, setSecondaryHeader] = useState("");
+	const { position, data, updateSideBar } = useSidebar();
+	const id = useRef("");
+	const catImage = useRef("");
+	const [mainHeader, setMainHeader] = useState("Dashboard");
+	const [secondaryHeader, setSecondaryHeader] = useState("");
 
-  useEffect(() => {
-    switch (position) {
-      case 0:
-        setMainHeader("Home");
-        break;
-      case 10:
-        setMainHeader("My Courses");
-        break;
-      case 11:
-        setMainHeader("Pending Courses");
-        break;
-      case 12:
-        setMainHeader("Completed Courses");
-        break;
-      case 13:
-        setMainHeader("Certifications");
-        break;
-      case 2:
-        setMainHeader("Case Studies");
-        break;
-      case 3:
-        setMainHeader("Knowledge Bank");
-        break;
+	useEffect(() => {
+		switch (position) {
+			case 0:
+				setMainHeader("Home");
+				break;
+			case 10:
+				setMainHeader("My Courses");
+				break;
+			case 11:
+				setMainHeader("Pending Courses");
+				break;
+			case 12:
+				setMainHeader("Completed Courses");
+				break;
+			case 13:
+				setMainHeader("Certifications");
+				break;
+			case 2:
+				setMainHeader("Case Studies");
+				break;
+			case 3:
+				setMainHeader("Knowledge Bank");
+				break;
 
-      default:
-        break;
-    }
-  }, [position]);
+			default:
+				break;
+		}
+	}, [position]);
 
-  const openCourse = (courseId: string, title: string) => {
-    id.current = courseId!;
-    setSecondaryHeader(title ?? "");
-    updateSideBar(data, COURSEDETAILINDEX);
-  };
+	const openCourse = (courseId: string, title: string) => {
+		id.current = courseId!;
+		setSecondaryHeader(title ?? "");
+		updateSideBar(data, COURSEDETAILINDEX);
+	};
 
-  const handleOnTopCardClick = (index: number) => {
-    switch (index) {
-      case 0:
-        updateSideBar(data, 3);
-        break;
-      case 1:
-        updateSideBar(data, 2);
-        break;
-      case 2:
-        updateSideBar(data, 13);
-        break;
+	const handleOnTopCardClick = (index: number) => {
+		const newData = data.map((item, pos) => {
+			if ((index === 0 && pos === 3) || (index === 1 && pos === 2)) {
+				return { ...item, isSelected: true };
+			} else if (pos === 1 && index == 2) {
+				return {
+					...item,
+					isSelected: true,
+					subItems:
+						item.subItems?.map((subItem, subPos) => {
+							return {
+								...subItem,
+								isSelected: subPos === 3,
+							};
+						}) || null,
+				};
+			}
+			return {
+				...item,
+				isSelected: false,
+				subItems:
+					item.subItems?.map((subItem) => ({
+						...subItem,
+						isSelected: false,
+					})) || null,
+			};
+		});
+		switch (index) {
+			case 0:
+				updateSideBar(newData, 3);
+				break;
+			case 1:
+				updateSideBar(newData, 2);
+				break;
+			case 2:
+				updateSideBar(newData, 13);
+				break;
 
-      default:
-        break;
-    }
-  };
+			default:
+				break;
+		}
+	};
 
-  const getComponent = useMemo(() => {
-    console.log(position);
-    switch (position) {
-      case 0:
-        return (
-          <DashboardHome
-            onCategoryCardClick={({ id: courseId, thumbnail, title }) => {
-              id.current = courseId!;
-              catImage.current = thumbnail;
-              setSecondaryHeader(title ?? "");
-              updateSideBar(data, 5);
-            }}
-            onCourseClick={({ id: courseId, title }) => {
-              openCourse(courseId!, title!);
-            }}
-            onTopCardsClick={handleOnTopCardClick}
-          />
-        );
-      case 2:
-        return <CaseStudyParent />;
-      case 3:
-        return <KnowledgeBankParent />;
+	const getComponent = useMemo(() => {
+		console.log(position);
+		switch (position) {
+			case 0:
+				return (
+					<DashboardHome
+						onCategoryCardClick={({ id: courseId, thumbnail, title }) => {
+							id.current = courseId!;
+							catImage.current = thumbnail;
+							setSecondaryHeader(title ?? "");
+							updateSideBar(data, 5);
+						}}
+						onCourseClick={({ id: courseId, title }) => {
+							openCourse(courseId!, title!);
+						}}
+						onTopCardsClick={handleOnTopCardClick}
+					/>
+				);
+			case 2:
+				return <CaseStudyParent />;
+			case 3:
+				return <KnowledgeBankParent />;
 
-      case 10:
-        return (
-          <AllCourses
-            url={createAPIEndpoint(`${ENROLLED}`)}
-            onCourseClick={({ id: courseId, title }) => {
-              openCourse(courseId!, title!);
-            }}
-          />
-        );
+			case 10:
+				return (
+					<AllCourses
+						url={createAPIEndpoint(`${ENROLLED}`)}
+						onCourseClick={({ id: courseId, title }) => {
+							openCourse(courseId!, title!);
+						}}
+					/>
+				);
 
-      case 11:
-        return (
-          <AllCourses
-            url={createAPIEndpoint(`${PENDING}`)}
-            onCourseClick={({ id: courseId, title }) => {
-              openCourse(courseId!, title!);
-            }}
-          />
-        );
+			case 11:
+				return (
+					<AllCourses
+						url={createAPIEndpoint(`${PENDING}`)}
+						onCourseClick={({ id: courseId, title }) => {
+							openCourse(courseId!, title!);
+						}}
+					/>
+				);
 
-      case 12:
-        return (
-          <AllCourses
-            url={createAPIEndpoint(`${COMPLETED}`)}
-            onCourseClick={({ id: courseId, title }) => {
-              openCourse(courseId!, title!);
-            }}
-          />
-        );
-      case 13:
-        return <CertificationsDashboard />;
-    }
-  }, [position]);
+			case 12:
+				return (
+					<AllCourses
+						url={createAPIEndpoint(`${COMPLETED}`)}
+						onCourseClick={({ id: courseId, title }) => {
+							openCourse(courseId!, title!);
+						}}
+					/>
+				);
+			case 13:
+				return <CertificationsDashboard />;
+		}
+	}, [position]);
 
-  return (
-    <main className="flex flex-col px-5 flex-1 my-5 overflow-auto">
-      {position === COURSEDETAILINDEX && (
-        <NavigationHeader
-          main={mainHeader}
-          secondary={secondaryHeader}
-          onClick={() => {
-            setSecondaryHeader("");
+	return (
+		<main className="flex flex-col px-5 flex-1 my-5 overflow-auto">
+			{position === COURSEDETAILINDEX && (
+				<NavigationHeader
+					main={mainHeader}
+					secondary={secondaryHeader}
+					onClick={() => {
+						setSecondaryHeader("");
 
-            updateSideBar(
-              data.map((data, pos) => {
-                if (pos === 0) {
-                  return {
-                    ...data,
-                    isSelected: true,
-                  };
-                }
-                return { ...data, isSelected: false };
-              }),
-              0
-            );
-          }}
-        />
-      )}
-      {getComponent}
-    </main>
-  );
+						updateSideBar(
+							data.map((data, pos) => {
+								if (pos === 0) {
+									return {
+										...data,
+										isSelected: true,
+									};
+								}
+								return { ...data, isSelected: false };
+							}),
+							0
+						);
+					}}
+				/>
+			)}
+			{getComponent}
+		</main>
+	);
 }
