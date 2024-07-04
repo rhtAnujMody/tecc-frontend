c4db303246cc4be304dcf35ad623366cb4600f0d
rhtAnujMody <anuj.mody@revealhealthtech.com>
2024-07-04 15:00:58 +0530

    changes to base url and image domain

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "d1gt2w0mc3uw79.cloudfront.net",
+        hostname: "d1rckynvzvnkn3.cloudfront.net",
         port: "",
       },
     ],
