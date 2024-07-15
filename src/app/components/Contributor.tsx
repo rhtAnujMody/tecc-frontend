2954c9baccd7ebd687accce893d14ca5b2952386
Augustine <Augustine.m@revealhealthtech.com>
2024-07-15 16:04:19 +0530

    minor change

--- src/app/components/Contributor.tsx
+++ src/app/components/Contributor.tsx
@@ -3,14 +3,12 @@ import { ContributorProps } from "@/types";
 import { formatContributors } from "@/lib/utils";
 
 const Contributor = ({ contributors }: ContributorProps) => {
-  const formattedContributors = formatContributors(contributors);
-
   return (
     <div className="mt-2 absolute bottom-2">
       <span className="text-xs text-text-secondary">
         {`Posted By: `}
         <span className="text-xs text-text-primary font-semibold capitalize">
-          {formattedContributors}
+          {formatContributors(contributors)}
         </span>
       </span>
     </div>
